9a5951d3a110919495af5988f5c1ab8a5030564e
Ayberk Mogol <ayberk.m@yandex.com>
2022-09-28 16:26:47 +0300

    Home View base style

---
+++ TraveLoop/TraveLoop/HomeModule/View/Cell/ArticlesCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ArticlesCollectionViewCell.swift
+//  TraveLoop
+//
+//  Created by Ayberk M on 28.09.2022.
+//
+
+import UIKit
+
+class ArticlesCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelTag: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- TraveLoop/TraveLoop/HomeModule/View/HomeViewController.swift
+++ TraveLoop/TraveLoop/HomeModule/View/HomeViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var banner: UIView!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var bg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeUI()
     }
 
-
 }
+
+private extension HomeViewController {
+    func makeUI() {
+        
+        bg.layer.shadowColor = UIColor(red: 0.138, green: 0.138, blue: 0.138, alpha: 0.4).cgColor
+        bg.layer.shadowOpacity = 1
+        bg.layer.shadowRadius = 20
+        bg.layer.shadowOffset = CGSize(width: 0, height: 5)
+        bg.layer.masksToBounds = false
+    }
+}
+
 

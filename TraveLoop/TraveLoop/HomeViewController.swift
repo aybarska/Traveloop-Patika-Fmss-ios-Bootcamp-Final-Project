a4cc75426a1d3416db61f5b3bd4d34a6bacc0f70
Ayberk Mogol <ayberk.m@yandex.com>
2022-09-28 12:29:39 +0300

    Project created

---
+++ TraveLoop/TraveLoop/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraveLoop
+//
+//  Created by Ayberk M on 28.09.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
